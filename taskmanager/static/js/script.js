c909100c23fba3567496b9e952868f0547d13b05
Squirkey89 <squirke89@gmail.com>
2022-04-27 17:29:20 +0000

    Add templates to mini project

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // sidenav initilisation
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
