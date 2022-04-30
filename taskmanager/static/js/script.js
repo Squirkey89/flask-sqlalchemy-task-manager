02b4afe3a3452e512e4c89e096c14b091a6262e8
Squirkey89 <squirke89@gmail.com>
2022-04-30 13:13:08 +0000

    Adding tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,19 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initilisation
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+  
+  // datepicker initilisation
+  let datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    i18n: {done: "Select"} 
   });
+
+  // select initilisation
+  let selects = document.querySelectorAll('select');
+  M.FormSelect.init(selects);
+
+  // collapsible initilisation
+  let collapsibles = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(collapsibles);
+});
